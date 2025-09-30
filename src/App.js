c44461944aa6bec0dd60517a1d30941c80beac8b
mascot111 <mascotahenkorah192@gmail.com>
2025-09-30 15:28:01 +0000

    Added fraud alert banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NetworkSelect from "./NetworkSelect";
@@ -8,13 +7,22 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<NetworkSelect />} />
-        <Route path="/payment/:network" element={<PaymentPage />} />
-        <Route path="/confirmation" element={<ConfirmationPage />} />
-      </Routes>
-    </Router>
+    <div>
+      {/* Fraud Alert Banner */}
+      <div className="alert-banner">
+        ⚠️ We have recently become aware of fraudulent individuals impersonating our company's repayment links in an attempt to deceive users into transferring funds to their accounts, resulting in financial losses for the users.  
+        To ensure the safety of your funds, please verify that the repayment link you are using is from our official channels.
+      </div>
+
+      {/* The rest of your site (Router) */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<NetworkSelect />} />
+          <Route path="/payment/:network" element={<PaymentPage />} />
+          <Route path="/confirmation" element={<ConfirmationPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
